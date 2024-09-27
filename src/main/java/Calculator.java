f31a5fbb1815a70a4b16f80bb9e57d4b6a32c2fc
zehra <Zehrag@metropolia.fi>
2024-09-27 09:49:45 +0300

    Kuvaus muutoksista

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,21 @@
+public class Calculator {
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public int divide(int a, int b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("Cannot divide by zero!");
+        }
+        return a / b;
+    }
+
+    public static void main(String[] args){
+        System.out.println("Done docker");
+    }
+}
